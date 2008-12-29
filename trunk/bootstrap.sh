68c3e979c0e72e0078976dff46d0d0db4d7b3f3a
jake@3.rdrail.net <jake@3.rdrail.net@fba13f4c-023d-0410-9ea0-c578ad9db326>
2008-12-29 03:23:42 +0000

    starting to rework the build

---
+++ trunk/bootstrap.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+#for macports
+if test -d /opt/local/share/aclocal; then
+  aclocal -I /opt/local/share/aclocal || exit 1
+else
+  aclocal || exit 1
+fi
+
+autoscan || exit 1
+autoheader || exit 1
+
+if libtoolize --version 1 >/dev/null 2>/dev/null; then
+  libtoolize --automake || exit 1
+elif glibtoolize --version 1 >/dev/null 2>/dev/null; then
+  glibtoolize --automake || exit 1
+fi
+
+autoconf
+automake -ac --add-missing --foreign || exit 1
+
+cd thrucommon; ./bootstrap.sh;
+cd ../thrudoc; ./bootstrap.sh;
+cd ../thrudex; ./bootstrap.sh;
+cd ../thruqueue; ./bootstrap.sh;
+
